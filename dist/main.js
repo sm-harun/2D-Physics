938f6ce25cfb52ab00d098c9ef35fe0307b8203f
sm-harun <harunyuss12@gmail.com>
2024-05-12 16:24:39 +0300

    Experimented with the radius

--- dist/main.js
+++ dist/main.js
@@ -7,19 +7,28 @@ canvas.height = window.innerHeight;
 x = canvas.width / 2;
 y = canvas.height / 2;
 
-vx = 9;
+vx = 8;
 vy = 9;
 
-radius = canvas.height / 10;
+radius = 40;
+radius_growth = 1;
 
 // We indicate direction using 1 and -1.
 directionY = 1;
 directionX = 1;
 
+aY = 0.4;
+
 function animate() {
 	
 	requestAnimationFrame(animate);
-	
+
+	if (radius < 10 || radius > 70) {
+		radius_growth = -radius_growth;
+	}
+
+	radius += radius_growth;
+
 	ctx.clearRect(0, 0, canvas.width, canvas.height);
 
 	ctx.beginPath();
@@ -31,10 +40,10 @@ function animate() {
 	ctx.stroke();
 	ctx.closePath();
 	
-	if (y >= (canvas.height - radius) || y <= radius) {
+	if (y > (canvas.height - radius) || y < radius) {
 		directionY = -directionY;
 	}
-	if (x >= (canvas.width - radius) || x <= radius) {
+	if (x > (canvas.width - radius) || x < radius) {
 		directionX = -directionX;
 	}
 	
